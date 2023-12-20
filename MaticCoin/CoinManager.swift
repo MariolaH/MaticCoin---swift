907e93a4ebfe702ac21d180011667a2cecf75e00
Mariola <mariolahullings@gmail.com>
2023-12-20 11:35:52 -0500

    added API_KEY to .plist

---
+++ MaticCoin/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  MaticCoin
+//
+//  Created by Mariola Hullings on 12/20/23.
+//
+
+import UIKit
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/MATIC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
